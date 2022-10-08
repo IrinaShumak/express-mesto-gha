c46bc64084b7440aaa2d0f41a9d0080ba7d116ae
Irina Shumak <spunovaiv@rambler.ru>
2022-10-09 02:41:42 +0300

    fix: error identification

--- controllers/cards.js
+++ controllers/cards.js
@@ -21,10 +21,14 @@ module.exports.createCard = (req, res) => {
 
 module.exports.deleteCard = (req, res) => {
   Card.findByIdAndRemove(req.params.cardId)
-    .then(() => res.send({ message: 'Пост удалён' }))
+    .then((card) => {
+      if (card) {
+        res.send({ data: 'Пост удалён' });
+      } else { res.status(ERROR_CODE_MISSING_ENTRY).send({ message: 'Передан несуществующий _id карточки.' }); }
+    })
     .catch((err) => {
-      if (err.name === 'CastError') {
-        res.status(ERROR_CODE_MISSING_ENTRY).send({ message: 'Карточка с указанным _id не найдена.' });
+      if ((err.name === 'ValidationError') || (err.name === 'CastError')) {
+        res.status(ERROR_CODE_INCORRECT_DATA).send({ message: 'Переданы некорректные данные для постановки лайка.' });
         return;
       }
       res.status(ERROR_CODE_OTHER).send({ message: 'Произошла ошибка' });
@@ -43,14 +47,14 @@ module.exports.likeCard = (req, res) => {
     { $addToSet: { likes: req.user._id } }, // добавить _id в массив, если его там нет
     { new: true },
   )
-    .then((card) => res.send({ data: card }))
+    .then((card) => {
+      if (card) {
+        res.send({ data: card });
+      } else { res.status(ERROR_CODE_MISSING_ENTRY).send({ message: 'Передан несуществующий _id карточки.' }); }
+    })
     .catch((err) => {
-      if (err.name === 'ValidationError') {
+      if ((err.name === 'ValidationError') || (err.name === 'CastError')) {
         res.status(ERROR_CODE_INCORRECT_DATA).send({ message: 'Переданы некорректные данные для постановки лайка.' });
-        return;
-      }
-      if (err.name === 'CastError') {
-        res.status(ERROR_CODE_MISSING_ENTRY).send({ message: 'Передан несуществующий _id карточки.' });
         return;
       }
       res.status(ERROR_CODE_OTHER).send({ message: 'Произошла ошибка' });
@@ -63,14 +67,14 @@ module.exports.dislikeCard = (req, res) => {
     { $pull: { likes: req.user._id } }, // убрать _id из массива
     { new: true },
   )
-    .then((card) => res.send({ data: card }))
+    .then((card) => {
+      if (card) {
+        res.send({ data: card });
+      } else { res.status(ERROR_CODE_MISSING_ENTRY).send({ message: 'Передан несуществующий _id карточки.' }); }
+    })
     .catch((err) => {
-      if (err.name === 'ValidationError') {
+      if ((err.name === 'ValidationError') || (err.name === 'CastError')) {
         res.status(ERROR_CODE_INCORRECT_DATA).send({ message: 'Переданы некорректные данные для снятия лайка.' });
-        return;
-      }
-      if (err.name === 'CastError') {
-        res.status(ERROR_CODE_MISSING_ENTRY).send({ message: 'Передан несуществующий _id карточки.' });
         return;
       }
       res.status(ERROR_CODE_OTHER).send({ message: 'Произошла ошибка' });
