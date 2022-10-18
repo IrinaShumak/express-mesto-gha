553cc6a8e0fcb6cb93566f53075eb5c90f6938af
Irina Shumak <spunovaiv@rambler.ru>
2022-10-18 22:15:34 +0300

    feature: login and signing controllers and routes auth using token centralized errors validation

--- controllers/cards.js
+++ controllers/cards.js
@@ -23,7 +23,7 @@ module.exports.deleteCard = (req, res, next) => {
   Card.findById(req.params.cardId)
     .then((card) => {
       if (card) {
-        if (card.owner === req.user._id) {
+        if (card.owner.toString === req.user._id) {
           card.delete()
             .then(() => res.send({ data: 'Пост удалён' }));
         } else {
